61b27ad62f49cf66417c4d38e312906bb9fffea1
dc <dc@log-ix.com>
2018-04-15 21:17:29 +0000

    dai wip

--- dai/db/form.sql
+++ dai/db/form.sql
@@ -1,20 +1,18 @@
 INSERT INTO dai.ilk (
   id,
   gem,
-  art,
   act,
   block,
   time,
   tx,
 )
 VALUES (
-  ${event.ilk},
+  ${event.id},
   ${event.gem},
-  0,
   'form',
   ${event.blockNumber},
   to_timestamp(${event.timestamp}),
   ${event.transactionHash}
 )
-ON CONFLICT ( id tx)
+ON CONFLICT (id)
 DO NOTHING
