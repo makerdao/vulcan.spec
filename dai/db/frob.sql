eaf99f743b0f798f61ed0127cecf3c59775c87f0
dc <dc@log-ix.com>
2018-04-15 21:51:29 +0000

    dai wip

--- dai/db/frob.sql
+++ dai/db/frob.sql
@@ -16,5 +16,23 @@ VALUES (
   to_timestamp(${event.timestamp}),
   ${event.transactionHash}
 )
-ON CONFLICT (ilk guy tx)
-DO NOTHING
+ON CONFLICT (ilk guy)
+DO UPDATE
+SET (
+  ilk,
+  guy,
+  ink,
+  art,
+  block,
+  time,
+  tx,
+) = (
+  ${event.ilk},
+  ${event.guy},
+  ${event.ink},
+  ${event.art},
+  ${event.blockNumber},
+  to_timestamp(${event.timestamp}),
+  ${event.transactionHash}
+)
+WHERE urn.ilk = ${event.id} AND urn.guy = ${event.guy};
--- dai/db/migrate/queries.sql
+++ dai/db/migrate/queries.sql
@@ -1,7 +1,38 @@
-/* CREATE VIEW dai.ilk AS */
-/*   SELECT * FROM ( */
-/*     SELECT DISTINCT ON (id) * */
-/*     FROM fuss.bid */
-/*     ORDER BY block DESC; */
-/*   ); t */
+-- TODO
+-- Computed columns
+CREATE VIEW public.urn AS
+  SELECT (
+    ilk,
+    guy,
+    ink,
+    art,
+    block,
+    time,
+    tx
+  )
+  FROM dai.urn
+  LEFT JOIN ilk on ilk.id = urn.ilk
+  ORDER BY block DESC;
 
+-- TODO
+-- Computed columns
+CREATE VIEW public.urn_history AS
+  SELECT (
+    ilk,
+    guy,
+    ink,
+    art,
+    block,
+    time,
+    tx
+  )
+  FROM dai.urn
+  LEFT JOIN ilk on ilk.id = urn.ilk
+  ORDER BY block DESC;
+
+CREATE OR REPLACE FUNCTION get_urn(id integer) RETURNS urn as $$
+  SELECT *
+  FROM urn
+  WHERE cup.id = $1
+  LIMIT 1
+$$ LANGUAGE SQL stable;
